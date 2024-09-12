b4b0c15b5a49fc98625241cf7d8c52f1eec43cbe
SIMRAN <118015518+deepsimrantechie@users.noreply.github.com>
2024-09-12 22:54:47 +0530

    added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>
 );
